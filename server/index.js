b39f7d0c6afc85e5d7dfc9f04d2365eab3c95119
Kyle Asaff <kyleasaff@pixleeteam.com>
2019-04-01 17:04:17 -0700

    cors

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // db Setup
 mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true });
 // App setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
